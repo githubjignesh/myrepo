62775856f97e58989ba045280ec82242e479e1bb
githubjignesh <patel.jignesh@hotmail.com>
2021-07-03 15:54:12 +0530

    To check Git changes

---
+++ Demo.java
@@ -0,0 +1,6 @@
+class Demo{
+	public static void main(String[] args){
+		System.out.println("First project for Git.");
+		System.out.println("Line added to checking for diff.");
+	}//end of main
+}//end of class
